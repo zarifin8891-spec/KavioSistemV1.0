ee7279797f10998636546ed7acdebab9014dfed0
zarifin8891-spec <zarifin8891@gmail.com>
2026-09-19 14:38:59 +0700

    refactor: standardize KAVIO back command

--- app/components/kavio-shell.tsx
+++ app/components/kavio-shell.tsx
@@ -114,7 +114,7 @@ export default function KavioShell({ children, active }: { children: React.React
               <h1>{title}</h1>
               <p>{subtitle}</p>
             </div>
-            {pathname.startsWith('/master/') && <Link href="/master" className="kavio-page-command"><span className="kavio-command-icon" aria-hidden="true">←</span><span>KEMBALI</span></Link>}
+            {pathname.startsWith('/master/') && <Link href="/master" className="kavio-command-button kavio-page-command"><span className="kavio-command-icon" aria-hidden="true">←</span><span>Kembali</span></Link>}
           </div>
         </header>
         <div className="kavio-content">{children}</div>
