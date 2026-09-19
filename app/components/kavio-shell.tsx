3e80b077ff9f8d9ff52733d74b00a28b887b10c5
zarifin8891-spec <zarifin8891@gmail.com>
2026-09-19 21:04:53 +0700

    style: remove back command from master kavling

--- app/components/kavio-shell.tsx
+++ app/components/kavio-shell.tsx
@@ -125,7 +125,7 @@ export default function KavioShell({ children, active }: { children: React.React
               <h1>{title}</h1>
               <p>{subtitle}</p>
             </div>
-            {pathname.startsWith('/master/') && !pathname.startsWith('/master/spk') && pathname !== '/master/sales' && (
+            {pathname.startsWith('/master/') && !pathname.startsWith('/master/spk') && pathname !== '/master/sales' && pathname !== '/master/kavling' && (
               <Link
                 href={pathname.startsWith('/master/sales/detail') ? '/master/sales' : '/master'}
                 className="kavio-command-button kavio-page-command"
