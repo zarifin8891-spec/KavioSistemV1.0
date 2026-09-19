7ac0d8715a5a3d457837c59c86e52385b56a6df8
zarifin8891-spec <zarifin8891@gmail.com>
2026-09-19 14:39:02 +0700

    refactor: normalize KAVIO command labels

--- app/components/KavioCreatePanel.tsx
+++ app/components/KavioCreatePanel.tsx
@@ -23,7 +23,7 @@ export default function KavioCreatePanel({
     <div className="kavio-create-wrap">
       <button type="button" className="kavio-command-button" onClick={() => setOpen((value) => !value)}>
         <span className="kavio-command-icon" aria-hidden="true">{open ? '×' : '+'}</span>
-        <span>{open ? closeLabel : buttonLabel.replace(/^\+\s*/, '')}</span>
+        <span>{open ? closeLabel.replace(/^[×+]?\s*/, '').toLowerCase().replace(/\b\w/g, (char) => char.toUpperCase()) : buttonLabel.replace(/^\+\s*/, '').toLowerCase().replace(/\b\w/g, (char) => char.toUpperCase())}</span>
       </button>
       {open && (
         <section className="kavio-panel kavio-create-panel">
