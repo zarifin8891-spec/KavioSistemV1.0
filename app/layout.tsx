ad2a53578fdcc72c37f3b7898069ab16eb6c2642
zarifin8891-spec <zarifin8891@gmail.com>
2026-09-19 10:49:18 +0700

    feat: register siteplan stylesheet

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './kavio-dashboard.css';
 import './kavio-sales-layout.css';
 import './kavio-sales-module.css';
 import './kavio-ui-components.css';
+import './siteplan/siteplan.css';
 
 export const metadata: Metadata = {
   title: 'KAVIO Monitor V1.0',
